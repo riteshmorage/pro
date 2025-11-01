dff8cf308b6eed6674e9dd5cbaddec17e4ebb7f3
Ritesh Morage <riteshmorage@gmail.com>
2025-11-01 22:06:27 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -222,7 +222,7 @@ def parse_opt(File):
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='show results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-csv', action='store_true', help='save results in CSV format')
+    parser.add_argument('--save-csv', action='store_true', default=True, help='save results in CSV format')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
